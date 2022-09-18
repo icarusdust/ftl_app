a7c08d7cf0217a0fcfcec8cef5ecd1b8eb2d706a
Nihal Agazade <43378371+icarusdust@users.noreply.github.com>
2022-09-18 18:45:50 +0300

    Update app_test.dart

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,20 +1,40 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:ftl_app/main.dart';
+
+import 'package:ftl_app/main.dart' as app;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('No input in name field displays error validation text',
-      (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
+  group('end-to-end test', () {
+    testWidgets('tap on the floating action button, verify counter',
+        (tester) async {
+      app.main();
+      await tester.pumpAndSettle();
 
-    await tester.tap(find.byType(FloatingActionButton));
-    await tester.pumpAndSettle();
+      // Verify the counter starts at 0.
+      expect(find.text('0'), findsOneWidget);
 
-    expect(find.byType(MyHomePage), findsOneWidget);
-    expect(find.text('Input some text!'), findsOneWidget);
+      // Finds the floating action button to tap on.
+      final Finder fab = find.byTooltip('Increment');
+
+      // Emulate a tap on the floating action button.
+      await tester.tap(fab);
+
+      // Trigger a frame.
+      await tester.pumpAndSettle();
+
+      // Verify the counter increments by 1.
+      expect(find.text('1'), findsOneWidget);
+    });
+
+    testWidgets('You shall not pass', (WidgetTester tester) async {
+        // Build our app and trigger a frame.
+        app.main();
+        await tester.pumpAndSettle();
+
+        expect(find.text('no such text'), findsOneWidget); // This will fail
+    });
   });
 }
 
